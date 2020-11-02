839a14482c7beb72db3e96c8c8e6b3b820f6a701
Rasha Zayed <zayed.rasha@outlook.com>
2020-11-02 11:32:41 -0800

    adding the details feature

--- server/controllers/productManager.controller.js
+++ server/controllers/productManager.controller.js
@@ -10,3 +10,24 @@ module.exports.createOne = (req, res) => {
     .then((productManager) => res.json({ productManager: productManager }))
     .catch((err) => res.json({ message: "Something went wrong", err }));
 };
+
+module.exports.getAll = (req,res)=> {
+  ProductManager.find({})
+  .then(product => res.json(product))
+  .catch(err => res.json(err))
+}
+module.exports.getOne = (req,res)=> {
+  ProductManager.findById({_id: req.params.id})
+  .then(product => res.json(product))
+  .catch(err => res.json(err))
+}
+module.exports.updateOne= (req, res) => {
+  ProductManager.findOneAndUpdate({_id: req.params.id} , req.body, {new:true})
+  .then(person => res.json(person))
+  .catch(err => res.json(err))
+}
+module.exports.deleteOne = (req,res)=> {
+  ProductManager.deleteOne({_id: req.params.id})
+  .then(person => res.json(person))
+  .catch(err=>res.json(err))
+}
--- server/routes/productManager.routes.js
+++ server/routes/productManager.routes.js
@@ -2,4 +2,8 @@ const productMangercontroller = require("../controllers/productManager.controlle
 
 module.exports = app => {
     app.post("/api/new" , productMangercontroller.createOne)
+    app.get("/api/products", productMangercontroller.getAll)
+    app.get("/api/products/:id",productMangercontroller.getOne )
+    app.put("/api/products/:id", productMangercontroller.updateOne)
+    app.delete("/api/prducts/:id",productMangercontroller.deleteOne)
 }
